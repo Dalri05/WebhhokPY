339c93b757bd28ebfd8f2ca808194e2069ac34f9
João Dalri <105470028+Dalri05@users.noreply.github.com>
2024-02-28 13:03:26 -0300

    Funções adiconadas
    Adicionado nome de usuario na mensagem.
    Adicionado função de save e delete webhhok.
    adicionado encurtadoir de url.
    adicionado embeds.

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 import requests
 import json
+import pyshorteners
 import pyautogui as bot
-from os import system
-from os import getlogin
+from os import system, remove, getlogin, path
 from time import sleep
 
-discord_webhook_url = input("Cole sua webhhok aqui: ")
+
+
 getlogin()
 username = getlogin()
 
+def newweb():
+    try:
+        webhook_url = input("Digite a URL da webhook: ")
+        with open('webhook.txt', 'w') as file:
+            file.write(webhook_url)
+        print("Webhook salva com sucesso")
+        system("cls")
+        main()
+        return webhook_url
+    except Exception as e:
+        print(f"Erro ao salvar a webhook: {str(e)}")
+        return None
 
-import requests
-import json
+def get_webhook_from_file():
+    try:
+        with open('webhook.txt', 'r') as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        return None
+
+discord_webhook_url = get_webhook_from_file() or newweb()
+
+def deleteweb():
+    try:
+        if path.exists('webhook.txt'):
+            remove('webhook.txt')
+            print("Webhhok removida")
+            system("cls")
+            main()
+        else:
+            print("O arquivo 'webhook.txt' não existe.")
+    except Exception as e:
+        print(f"Erro ao excluir': {str(e)}")
 
 def msgwebhook():
     message_content = input("Digite sua mensagem: ")
@@ -36,14 +67,13 @@ def msgwebhook():
         print('Mensagem enviada com sucesso para o Discord!')
         op = input("Deseja enviar mais mensagens? (sim/não): ")
         if op.lower() == "sim":
+            system("cls")
             msgwebhook()
         else:
+            system("cls")
             main()
     else:
         print(f'Erro ao enviar mensagem para o Discord. Código de status: {response.status_code}')
-
-
-
 
 def spam():
     quantmsg = int(input("Digite a quantidade de mensagens que deseja enviar: "))
@@ -74,6 +104,34 @@ def imgtela():
         system("cls")
         main()
 
+def shorturl():
+    url = input("cole seu url aqui: ")
+    shortener = pyshorteners.Shortener()
+    short_url = shortener.tinyurl.short(url)
+
+    embed = {
+        "title": "Aqui esta seu URL: ",
+        "description": short_url,
+        "color": 16711680 
+    }
+
+    payload = {
+        "embeds": [embed]
+    }
+
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(discord_webhook_url, data=json.dumps(payload), headers=headers)
+
+    if response.status_code == 204:
+        print("Url enviada com sucesso, retornando ao menu...")
+        sleep(4)
+        system("cls")
+        main()
+    else:
+        print(f'Erro ao enviar mensagem para o Discord. Código de status: {response.status_code}')
 
 def main():
     print('''
@@ -82,6 +140,12 @@ def main():
     [1] - Enviar mensagem via WebHook
     [2] - Spam de webhook
     [3] - Mandar print da tela 
+    [4] - Encurtador de URL
+    [5] - Ver github
+    -----------------------------------
+    [8] - cadastrar webhook
+    [9] - remover webhook
+    [10] - sair
     ''')
     op = int(input(":"))
     if op == 1:
@@ -90,6 +154,16 @@ def main():
         spam()
     elif op == 3:
         imgtela()
+    elif op == 4:
+        shorturl()
+    elif op == 5:
+        print()
+    elif op == 8:
+        newweb()
+    elif op == 9:
+        deleteweb
+    elif op == 10:
+        exit
     else:
         print("Opção incorreta")
 
